2648744ec8a0839de0f435f1640e322c2028dc81
danik <danikpon123@gmail.com>
2019-10-03 22:02:45 +0300

    0

---
+++ 5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double xt, x, v, t, a, g=-9.8;
+	cout << "Введите значение x0, v0, t: \n";
+	cin >> x >> v >> t;
+	a = v / t;
+	xt = x + (v * t) + (a * t * t) / 2;
+	
+	cout << xt;
+	return 0;
+}
